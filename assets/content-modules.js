4e7e15611b5f9d087ca9271e2145c1996a2023f1
Harry <harry750110@gmail.com>
2026-06-23 18:55:59 +0800

    Updates

--- assets/content-modules.js
+++ assets/content-modules.js
@@ -1,7 +1,7 @@
 const contentModules = new Map([
 ["src/content/styles/ghibli.mdx", () => import('./ghibli.js')],
-["src/content/styles/makoto-shinkai.mdx", () => import('./makoto-shinkai.js')],
 ["src/content/styles/pixar.mdx", () => import('./pixar.js')],
+["src/content/styles/makoto-shinkai.mdx", () => import('./makoto-shinkai.js')],
 ["src/content/styles/pixel-art.mdx", () => import('./pixel-art.js')],
 ["src/content/styles/watercolor.mdx", () => import('./watercolor.js')]]);
 
